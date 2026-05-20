6cd46c568869b54c86e6e720390875bd4c83fb7a
TBluiz03 <ederlinstel@gmail.com>
2026-05-20 17:26:47 -0300

    feat: adição das validações em prédios e formulários e métodos feedback

--- SOSBackEndClean/SOSBackClean.Domain/Entities/Formulario.cs
+++ SOSBackEndClean/SOSBackClean.Domain/Entities/Formulario.cs
@@ -13,10 +13,10 @@ namespace SOSBackClean.Domain.Entities
         public int Id { get; private set; }
         public bool _statusResolucao { get; private set; } = false;
         public string _protocolo { get; private set; }
-        public string? _nomeOfendido { get; private set; }
+        public string? _nomeOfendido { get; private set; } 
         public string? _sala { get; private set; }
         public int _andarOcorrencia { get; private set; }
-        public INFRATOR _infrator { get; private set; }
+        public INFRATOR _infrator { get; private set; }        
         public INFRACAO _tipoInfracao { get; private set; }
         public string _descricao { get; private set; }
         public string? _arquivo { get; private set; }
@@ -38,21 +38,60 @@ namespace SOSBackClean.Domain.Entities
             INFRACAO infracao,
             string descricao,
             string? arquivo,
-            int predio_id, int funcionario_id)
+            int predio_id, int funcionario_id) // ???? 
         {
-            Validation();
+            Validation(protocolo, nomeOfendido, sala, andarOcorrencia, infracao, descricao, arquivo);
 
             _dataCriacao = DateTime.Now;
         }
 
 
-        public void Validation()
+        public void Validation(string protocolo, string? nomeOfendido, string? sala, int andarOcorrencia, INFRACAO infracao, string descricao, string? arquivo)
         {
+            //Criar validações
+            //Criar validação para verificar se o protocolo é único (Luiz).
+
+            DomainValidation.When(string.IsNullOrEmpty(protocolo), "O protocolo é obrigatório.");
+            DomainValidation.When(protocolo.Length != 10, "O protocolo precisa ter 10 caracteres.");
+            DomainValidation.When(int.IsNullOrEmpty(andarOcorrencia), "O andar da ocorrência é obrigatório.");
+            DomainValidation.When(andarOcorrencia < 0, "O andar da ocorrência não pode ter valores negativos.");
+            DomainValidation.When(INFRACAO.IsNullorEmpty(infracao), "O tipo de infração é obrigatório.");
+            DomainValidation.When(string.IsNullOrEmpty(descricao), "A descrição da ocorrência é obrigatória.");
             
-           //Criar validações
-            
+            _protocolo = protocolo;
+            _nomeOfendido = nomeOfendido;
+            _sala = sala;
+            _andarOcorrencia = andarOcorrencia;
+            _tipoInfracao = infracao;
+            _descricao = descricao;
+            _arquivo = arquivo;
+            _nomeOfendido = nomeOfendido; 
+
         }
 
         //Precisamos de métodos para atualizar feedback 
+        public void ResolverOcorrencia(string feedback)
+        {
+            DomainValidation.When(IsNullorEmpty(feedback), "O feedback é obrigatório.");
+
+            _feedBack = feedback;
+            _statusResolucao = true;
+        }
+
+        public void ReabrirOcorrencia()
+        {
+            _statusResolucao = false;
+        }
+
+        public void AssociaInfrator(INFRATOR infrator) {
+
+            DomainValidation.When(INFRATOR.IsNullorEmpty(infrator), "O infrator é obrigatório.");
+
+            _infrator = infrator;
+        }
+        public void AlterarInfrator(INFRATOR novoInfrator)
+        {
+            _infrator = novoInfrator;
+        }
     }
 }
--- SOSBackEndClean/SOSBackClean.Domain/Entities/Predio.cs
+++ SOSBackEndClean/SOSBackClean.Domain/Entities/Predio.cs
@@ -11,17 +11,26 @@ namespace SOSBackClean.Domain.Entities
     {
         public int Id { get; set; }
         public string _nome { get; set; }
-        public int _nAndar {  get; set; }
-        
+        public int _nAndar { get; set; }
+
         public ICollection<Formulario> formularios = new List<Formulario>();
 
         public Predio(int andar, string nome)
         {
             //Falta colocar as validações (Luiz).
+            Validation(nome, andar);
+        }
+
+        public void Validation(string nome, int andar)
+        {
+            DomainValidation.When(string.IsNullOrEmpty(nome), "O nome do prédio é obrigatório.");
+            DomainValidation.When(nome.Length < 3, "O nome do prédio não pode ter menos de 3 letras. ");
+            DomainValidation.When(int.IsNullOrEmpty(andar), "O prédio precisa ter andares.");
+            DomainValidation.When(andar < 0, "O andar não pode ter andares com valores negativos. ");
+            DomainValidation.When(andar == 0, "O prédio precisa ter andares.");
 
             _nome = nome;
             _nAndar = andar;
-   
         }
     }
 }
