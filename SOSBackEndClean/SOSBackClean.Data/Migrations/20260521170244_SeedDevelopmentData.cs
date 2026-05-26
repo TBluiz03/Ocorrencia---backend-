0b3616b0955987798e6ab40147193fb4538e57f7
Pedro Gabriel Aiello <158206187+xPG23@users.noreply.github.com>
2026-05-26 17:20:54 -0300

    Commit pré deploy, fix> tabelas com nome errado para o Deploy

--- SOSBackEndClean/SOSBackClean.Data/Migrations/20260521170244_SeedDevelopmentData.cs
+++ SOSBackEndClean/SOSBackClean.Data/Migrations/20260521170244_SeedDevelopmentData.cs
@@ -10,12 +10,12 @@ namespace SOSBackClean.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("ALTER TABLE predio MODIFY COLUMN Nome nvarchar(20) Not Null Unique");
+            migrationBuilder.Sql("ALTER TABLE Predio MODIFY COLUMN Nome nvarchar(20) Not Null Unique");
 
 
-            migrationBuilder.Sql("INSERT INTO predio(Nome, Numero_Andares) VALUES('Predio Sul', 4)");
-            migrationBuilder.Sql("INSERT INTO predio(Nome, Numero_Andares) VALUES('Predio Norte', 3)");
-            migrationBuilder.Sql("INSERT INTO predio(Nome, Numero_Andares) VALUES('Predio Leste', 2)");
+            migrationBuilder.Sql("INSERT INTO Predio(Nome, Numero_Andares) VALUES('Predio Sul', 4)");
+            migrationBuilder.Sql("INSERT INTO Predio(Nome, Numero_Andares) VALUES('Predio Norte', 3)");
+            migrationBuilder.Sql("INSERT INTO Predio(Nome, Numero_Andares) VALUES('Predio Leste', 2)");
 
             migrationBuilder.Sql("INSERT INTO Funcionario(Nome, Foto, Cargo) VALUES('Marcelo','https:/12h?392fkdj',3)");
             migrationBuilder.Sql("INSERT INTO Funcionario(Nome, Foto, Cargo) VALUES('fatima','',3)");
