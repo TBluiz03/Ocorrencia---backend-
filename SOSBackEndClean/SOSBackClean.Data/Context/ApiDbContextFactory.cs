9c3dabe1fdb7729537d65e9bb6f6fc4dfe4abbb9
Pedro Gabriel Aiello <158206187+xPG23@users.noreply.github.com>
2026-05-26 14:55:41 -0300

    Commit pré deploy 2 fix: ContexFactory + Settings para ambiente de produção
    ContexFactory> Devido ao Dependency Injection (DI) o construtor do dbContext foi alterado nos últimos commits, e para realizar migrations o código fora de tempo de execução chama o construtor, mas devido ao construtor não teria como acessar sem uma Factory para isso.

---
+++ SOSBackEndClean/SOSBackClean.Data/Context/ApiDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSBackClean.Data.Context
+{
+    public class ApiDbContextFactory
+        : IDesignTimeDbContextFactory<ApiDbContext>
+    {
+        public ApiDbContext CreateDbContext(
+            string[] args)
+        {
+            var connectionString =
+                "server=localhost;database=TestDb;user=root;password=root123";
+
+            var optionsBuilder =
+                new DbContextOptionsBuilder<ApiDbContext>();
+
+            optionsBuilder.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString));
+
+            return new ApiDbContext(
+                optionsBuilder.Options);
+        }
+    }
+}
