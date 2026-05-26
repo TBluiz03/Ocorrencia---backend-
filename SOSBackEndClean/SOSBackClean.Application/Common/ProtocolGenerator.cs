4d72c537e948049688f5c35a703e66f2685421ff
TBluiz03 <ederlinstel@gmail.com>
2026-05-26 17:00:59 -0300

    feat: adiciona gerador de protocolo + conserta validações
    Necessário mudar a estrutura e começar a tratar funcionario_id podendo ser null

--- SOSBackEndClean/SOSBackClean.Application/Common/ProtocolGenerator.cs
+++ SOSBackEndClean/SOSBackClean.Application/Common/ProtocolGenerator.cs
@@ -16,11 +16,23 @@ namespace SOSBackClean.Application.Common
 
         public async Task<string> GenerateProtocolAsync()
         {
+            
             string protocol;
 
             //Parte pro luiz fazer
-            protocol = "Quem é esse neguin?? ";
 
+            bool Existe = false;
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string numbers = "0123456789";
+            do
+            {
+                Random random = new Random();
+
+                protocol = "#" + new string(Enumerable.Repeat(chars, 3).Select(s => s[random.Next(s.Length)]).ToArray()) +
+                    new string(Enumerable.Repeat(numbers, 7).Select(s => s[random.Next(s.Length)]).ToArray());
+
+                Existe = await _FormularioRepo.ExistsProtocol(protocol);
+            } while (Existe);
 
             return protocol;
 
